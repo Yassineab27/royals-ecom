8ba93a640803e6d5dfbe41de5391c9b6dc07503e
Yassineab27 <45517237+Yassineab27@users.noreply.github.com>
2019-09-13 18:34:11 +0100

    change directery name

---
+++ src/components/home-menu/MenuSec.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "react-redux";
+import MenuItem from "./MenuItem";
+import { selectSecSections } from "../selectors/menuSecDataSelectors";
+
+const MenuSec = props => {
+  const sections = props.sections.map(section => (
+    <MenuItem key={section.id} section={section} />
+  ));
+  return <div className="menu-secondary">{sections}</div>;
+};
+
+const mapStateToProps = state => {
+  return { sections: selectSecSections(state) };
+};
+
+export default connect(mapStateToProps)(MenuSec);
