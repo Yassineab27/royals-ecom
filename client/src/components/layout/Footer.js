759b2add472a24243cda3b21fca7e6d2ab7a109d
Yassineab27 <45517237+Yassineab27@users.noreply.github.com>
2019-09-21 10:50:56 +0100

    styling footer in home, shop and collection page

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <i className="fas fa-crown fa-4x logo text-center" />
+      <ul className="options">
+        <li>
+          <NavLink
+            to="/shop"
+            className="option"
+            activeClassName="active-option"
+          >
+            SHOP
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="option"
+            to="/contact"
+            activeClassName="active-option"
+          >
+            CONTACT
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="option"
+            to="/checkout"
+            activeClassName="active-option"
+          >
+            CHECKOUT
+          </NavLink>
+        </li>
+      </ul>
+      <h4 className="text-center">
+        Copyright &copy; 2019 Royals - All Rights Reserved{" "}
+      </h4>
+    </div>
+  );
 };
 
 export default Footer;
