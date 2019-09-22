b856bdda0c284d7a1be4f2a2043bd4e539538b27
Yassineab27 <45517237+Yassineab27@users.noreply.github.com>
2019-09-22 14:37:50 +0100

    adding some responsive styling to the checkout and confirmation pages, removing unnecessary code

--- client/src/components/selectors/cartSelectors.js
+++ client/src/components/selectors/cartSelectors.js
@@ -4,25 +4,6 @@ import { createSelector } from "reselect";
 const selectCart = state => state.cart;
 
 // OUTPUT SELECTOR => Memoized selector
-export const selectCartItems = createSelector(
-  [selectCart],
-  cart => cart.cartItems
-);
-
-export const selectCartItemsCount = createSelector(
-  [selectCartItems],
-  cartItems => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
-);
-
-export const selectCartTotalPrice = createSelector(
-  [selectCartItems],
-  cartItems =>
-    cartItems.reduce(
-      (acc, cartItem) => acc + cartItem.price * cartItem.quantity,
-      0
-    )
-);
-
 export const selectCartHidden = createSelector(
   [selectCart],
   cart => cart.cartHidden
--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -7,31 +7,6 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "REMOVE_QUANTITY":
-      return {
-        ...state,
-        cartItems: state.cartItems.map(cartItem => {
-          if (cartItem.id === action.payload) {
-            return cartItem.quantity > 1
-              ? { ...cartItem, quantity: cartItem.quantity - 1 }
-              : { ...cartItem, quantity: 1 };
-          }
-
-          return cartItem;
-        })
-      };
-    case "DELETE_ITEM":
-      return {
-        ...state,
-        cartItems: state.cartItems.filter(
-          cartItem => cartItem.id !== action.payload
-        )
-      };
-    case "ADD_ITEM":
-      return {
-        ...state,
-        cartItems: addItemsFilter(state.cartItems, action.payload)
-      };
     case "TOGGLE_CART_DROPDOWN":
       return { ...state, cartHidden: !state.cartHidden };
     default:
